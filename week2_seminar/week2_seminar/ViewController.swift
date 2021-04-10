6f57377d2cb4ec84b8be443bdfbd3a3d829057a8
김윤서 <yo26on@soongsil.ac.kr>
2021-04-10 20:20:22 +0900

    [ADD] week2_seminar

---
+++ week2_seminar/week2_seminar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2_semina
+//
+//  Created by 김윤서 on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
